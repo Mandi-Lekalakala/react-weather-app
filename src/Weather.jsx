91c5e865263600c4db1f88a4b37cfba0c4d2518a
Mandi-Lekalakala <mandilekalakala@gmail.com>
2026-09-24 21:32:18 +0200

    integrated shecodes weather api and made a few changes

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,59 +1,88 @@
+import { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 export default function Weather() {
-  const apiKey = "cfabea60f0eoe0t3bb3e16a70e85e14b";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  return (
-    <div className="Weather">
-      {/* Search Bar & Button */}
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      city: response.data.city,
+      date: "Wednesday 19:00",
+      description: response.data.condition.description,
+      iconUrl:
+        "https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_night_light.svg",
+    });
+  }
 
-      <form className="row">
-        <div className="col-10">
-          <input
-            type="search"
-            placeholder="Enter a city.."
-            className="search-input"
-            autoFocus
-          />
-        </div>
-        <div className="col-2">
-          <input type="submit" value="Search" className="search-button" />
-        </div>
-      </form>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        {/* Search Bar & Button */}
 
-      {/* Current Location and Info */}
+        <form className="row">
+          <div className="col-10">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="search-input"
+              autoFocus
+            />
+          </div>
+          <div className="col-2">
+            <input type="submit" value="Search" className="search-button" />
+          </div>
+        </form>
 
-      <div className="row weather-card align-items-center">
-        <div className="col-9">
-          <h1>Johannesburg</h1>
-          <p className="weather-date">Wed 14:20, clear sky</p>
+        {/* Current Location and Info */}
+
+        <div className="row weather-card align-items-center">
+          <div className="col-9">
+            <h1>{weatherData.city}</h1>
+            <p className="weather-date text-capitalize">
+              {weatherData.date}, {weatherData.description}
+            </p>
+          </div>
+          <div className="col-3 text-center">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              className="weather-icon"
+            />
+            <div className="weather-temp">
+              {Math.round(weatherData.temperature)}
+              <span className="weather-unit">°C</span>
+            </div>
+          </div>
         </div>
-        <div className="col-3 text-center">
-          <img
-            src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
-            alt="clear sky"
-            className="weather-icon"
-          />
-          <div className="weather-temp">24°</div>
+
+        {/* Current Weather Stats */}
+
+        <div className="weather-stats">
+          <div className="stat-card text-center">
+            <p className="stat-label">Humidity</p>
+            <p className="stat-value">{Math.round(weatherData.humidity)}%</p>
+          </div>
+          {/* <div className="stat-card text-center">
+            <p className="stat-label">Precipitation</p>
+            <p className="stat-value">15%</p>
+          </div> */}
+          <div className="stat-card text-center">
+            <p className="stat-label">Wind</p>
+            <p className="stat-value">{Math.round(weatherData.wind)} km/h</p>
+          </div>
         </div>
       </div>
+    );
+  } else {
+    const apiKey = "cfabea60f0eoe0t3bb3e16a70e85e14b";
+    let city = "Pretoria";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
 
-      {/* Current Weather Stats */}
-
-      <div className="weather-stats">
-        <div className="stat-card text-center">
-          <p className="stat-label">Humidity</p>
-          <p className="stat-value">40%</p>
-        </div>
-        <div className="stat-card text-center">
-          <p className="stat-label">Precipitation</p>
-          <p className="stat-value">15%</p>
-        </div>
-        <div className="stat-card text-center">
-          <p className="stat-label">Wind</p>
-          <p className="stat-value">10 km/h</p>
-        </div>
-      </div>
-    </div>
-  );
+    return "Loading....";
+  }
 }
